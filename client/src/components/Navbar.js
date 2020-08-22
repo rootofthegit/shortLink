8602591af37285ac060537804a46b96dc7e8a498
Denys Kaspryshen <rootofthemail@gmail.com>
2020-08-22 22:09:10 +0300

    initial

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React, {useContext} from "react";
+import {NavLink, useHistory} from "react-router-dom";
+import {AuthContext} from "../context/AuthContext";
+
+export const Navbar = () => {
+    const history = useHistory()
+    const auth = useContext(AuthContext)
+
+    const logoutHandler = event => {
+        event.preventDefault()
+        auth.logout()
+        history.push('/')
+    }
+
+    return (
+        <nav>
+            <div className="nav-wrapper red darken-3" style={{padding: '0 2rem'}}>
+                <span className="brand-logo">NeuroLink =)</span>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <li><NavLink to="/create">Coздать</NavLink></li>
+                    <li><NavLink to="/links">Список ссылок</NavLink></li>
+                    <li><a href="/" onClick={logoutHandler}>Выйти</a></li>
+
+                </ul>
+            </div>
+        </nav>
+    )
+}
